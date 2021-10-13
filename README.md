15360ebe5b0715e7b3ca899fbf58e1604d267bf4
weeraponak <weerapon.ak.61@ubu.ac.th>
2021-10-13 17:32:06 +0700

    success

---
+++ README.md
@@ -0,0 +1,26 @@
+"# Poke-Card" 
+## Project setup
+```
+git clone https://github.com/Weeraponak/Poke-Card
+npm install -g yarn
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve || docker-compose up
+```
+
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <main-screen v-if="statusMatch === 'default'" @onStart="onHandleBeforeStart($event)" />
+  <interact-screen v-if="statusMatch === 'match'" :cardsContext="settings.cardsContext" @onFinnish="onGetResult" />
+  <result-screen v-if="statusMatch === 'result'" :timer="timer" @onStartAgain="statusMatch = 'default'"  />
+</template>
+
+<script>
+import MainScreen from "./components/MainScreen.vue";
+import InteractScreen from "./components/InteractScreen.vue";
+import ResultScreen from "./components/ResultScreen.vue";
+import { shuffled } from "./utils/array";
+
+export default {
+  name: "App",
+    components: {
+    MainScreen,
+    InteractScreen,
+    ResultScreen,
+  },
+  data() {
+    return {
+      settings: {
+        totalOfBlocks: 0,
+        cardsContext: [],
+        startedAt: null,
+      },
+      statusMatch: "default",
+      timer: 0,
+    };
+
+  },
+
+  methods: {
+    onHandleBeforeStart(config) {
+      console.log("running handle before start,", config);
+      this.settings.totalOfBlocks = config.totalOfBlocks;
+
+      const firstCards = Array.from(
+        { length: this.settings.totalOfBlocks / 2}, 
+        (_, i) => i + 1
+      );
+      const secondCards = [...firstCards];
+      const cards = [...firstCards, ...secondCards];
+      console.log(cards);
+      this.settings.cardsContext = shuffled(shuffled(shuffled(shuffled(cards)))
+      );
+      this.settings.startedAt = new Date().getTime();
+
+
+      // data ready
+      this.statusMatch = "match";
+    },
+
+    onGetResult() {
+      // get timer
+      this.timer = new Date().getTime() - this.settings.startedAt;
+
+      // switch to result component
+      this.statusMatch = "result";
+    },
+  },
+};
+</script>
+
+
